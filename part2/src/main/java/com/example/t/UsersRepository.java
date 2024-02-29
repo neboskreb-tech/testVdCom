a3ea210a49b03d7269464ea50a66fd73b94edd2a
neboskreb-tech <suhov.toxa@gmail.com>
2024-02-29 13:00:21 +0400

    Revert "changed UsersController and UsersRepository"
    This reverts commit 7b0a05ec33669e4fbc01a33c52160816b6978886.

--- part2/src/main/java/com/example/t/UsersRepository.java
+++ part2/src/main/java/com/example/t/UsersRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UsersRepository extends JpaRepository<Users,Long> {
-  boolean existsByFirstNameAndSecondNameAndLastName(String firstName, String secondName, String lastName);
+  boolean findByFirstNameAndSecondNameAndLastName(String firstName, String secondName, String lastName);
 }
